16caead35463773411ac0e2151a10f2c2354a996
Yaroslav Volohov <yvolohov@gmail.com>
2014-10-09 00:14:29 +0300

    third commit

--- public_html/js/seaWar.js
+++ public_html/js/seaWar.js
@@ -1,61 +1,149 @@
 function yvSeaWar()
-{
-    this.humanField = this.getEmptyField();
-    this.computerField = this.getEmptyField();
+{     
+    this.fieldWidth = 10;
+    this.fieldHeight = 10;
+    this.ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
     
-    this.setShips(this.humanField);
-    console.log(this.cellExist(3, 3));
-    console.log(this.cellExist(3, 9));    
+    this.humanField = this.model.getEmptyField(this);
+    this.computerField = this.model.getEmptyField(this);
+    this.model.setShips(this, this.computerField);
+    this.view.showField(this, this.computerField);
 }
 
-yvSeaWar.prototype = 
+yvSeaWar.prototype = {};
+
+yvSeaWar.prototype.model =
 {
-    FIELD_WIDTH : 10,
-    FIELD_HEIGHT : 10,
-    SHIPS : [4, 3, 3, 2, 2, 2, 1, 1, 1, 1],
-    
-    getEmptyField : function()
+    getEmptyField : function(cont)
     {
         var field = new Array();
-        
-        for (var y = 0; y < this.FIELD_HEIGHT; y++)
+
+        for (var y = 0; y < cont.fieldHeight; y++)
         {
             field[y] = new Array();
-            
-            for (var x = 0; x < this.FIELD_WIDTH; x++)
+
+            for (var x = 0; x < cont.fieldWidth; x++)
                 {field[y][x] = 0;}
         }       
         return field;
     },
-    
-    getCell : function(field, posX, posY)
+
+    cellExist : function(cont, posX, posY)
     {
-        if (!this.cellExist(posX, posY))
+        return (posX >= 0 && posY >= 0 && 
+                posX < cont.fieldWidth && 
+                posY < cont.fieldHeight);
+    },
+
+    getCell : function(cont, field, posX, posY)
+    {
+        if (!(cont.model.cellExist(cont, posX, posY)))
             {return -1;}
-            
+
         return field[posY][posX];
     },
     
-    cellExist : function(posX, posY)
+    setCell : function(cont, field, posX, posY, value)
     {
-        return (posX >= 0 && posY >= 0 && 
-                posX < this.FIELD_WIDTH && 
-                posY < this.FIELD_HEIGHT);
+        if (!(cont.model.cellExist(cont, posX, posY)))
+            {return;}
+        
+        field[posY][posX] = value;
     },
     
-    setShips : function(field)
+    setCells : function(cont, field, position, value)
     {
-        for (var count = 0; count < this.SHIPS.length; count++)
+        for (var y = position.y1; y <= position.y2; y++)
         {
-            var ship = this.SHIPS[count];
+            for (var x = position.x1; x <= position.x2; x++)
+            {
+                cont.model.setCell(cont, field, x, y, value);
+            }
+        }        
+    },
+
+    setShips : function(cont, field)
+    {
+        for (var count = 0; count < cont.ships.length; count++)
+        {
+            var ship = cont.ships[count];
             var direction = Math.round(Math.random());
-            this.setShip(field, ship, direction);
+            cont.model.setShip(cont, field, ship, direction);
         }
     },
-    
-    setShip : function(field, ship, direction)
+
+    setShip : function(cont, field, ship, direction)
     {
-        console.log(String(ship));
+        var offsetX = (direction === 1) ? ship - 1 : 0;
+        var offsetY = (direction === 0) ? ship - 1 : 0;
+        var width = cont.fieldWidth - offsetX;
+        var height = cont.fieldHeight - offsetY;
+        var shipBody = {x1 : 0, y1 : 0, x2 : offsetX, y2 : offsetY};
+        var shipSpace = {x1 : 0, y1 : 0, x2 : 0, y2 : 0};
+        var positions = [];
+        
+        for (var y = 0; y < height; y++)
+        {           
+            for (var x = 0; x < width; x++)
+            {
+                if (cont.model.checkShipSpace(cont, field, shipBody, shipSpace))
+                {
+                    positions[positions.length] = 
+                    {
+                        x1 : shipBody.x1, 
+                        y1 : shipBody.y1, 
+                        x2 : shipBody.x2, 
+                        y2 : shipBody.y2
+                    };
+                }
+                shipBody.x1++;
+                shipBody.x2++;                
+            }          
+            shipBody.y1++;
+            shipBody.y2++;
+            shipBody.x2 = shipBody.x2 - shipBody.x1;
+            shipBody.x1 = 0;            
+        }
+        var index = Math.floor(Math.random() * positions.length);
+        var position = positions[index];
+        cont.model.setCells(cont, field, position, 1);
+    },
+    
+    checkShipSpace : function(cont, field, shipBody, shipSpace)
+    {
+        shipSpace.x1 = shipBody.x1 - 1;
+        shipSpace.y1 = shipBody.y1 - 1;
+        shipSpace.x2 = shipBody.x2 + 1;
+        shipSpace.y2 = shipBody.y2 + 1;
+        
+        for (var y = shipSpace.y1; y <= shipSpace.y2; y++)
+        {
+            for (var x = shipSpace.x1; x <= shipSpace.x2; x++)
+            {
+                if (cont.model.getCell(cont, field, x, y) > 0)
+                    {return false;}
+            }
+        }
+        return true;
+    }
+};             
+
+yvSeaWar.prototype.view = 
+{
+    /* test view */
+    showField : function(cont, field)
+    {
+        var str = "";
+        
+        for (var y = 0; y < cont.fieldHeight; y++)
+        {
+            for (var x = 0; x < cont.fieldWidth; x++)
+            {                
+                str = str + "  " + cont.model.getCell(cont, field, x, y);                       
+            }
+            console.log(y + " : " + str);
+            str = "";
+        }        
     }
 };
 
